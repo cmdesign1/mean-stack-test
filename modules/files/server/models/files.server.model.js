59d600285e2cadcd78bbbd158bdd804587220787
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-06-21 15:49:06 +0100

    Update file model.

---
+++ modules/files/server/models/files.server.model.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * File Schema
+ */
+var FileSchema = new Schema({
+  name: {
+    type: String
+  }
+  type: {
+    type: String
+  }
+  owner: {
+    type: [{
+      type: Schema.Types.ObjectId
+    }]
+  },
+  author: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  /* Content */
+  desc: {
+    type: String,
+    default: ''
+  },
+  profileImageURL: {
+    type: String,
+    default: 'modules/users/client/img/profile/default.png'
+  },
+  /* Timestamps */
+  updated: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  /* Relational */
+  messages: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Message'
+    }]
+  }
+});
+
+mongoose.model('File', FileSchema);
