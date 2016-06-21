dd57614cf1f3784b0c4803849c9fb2f106c099aa
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-06-21 13:44:53 +0100

    Add project client service.

---
+++ modules/projects/client/services/projects.client.service.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('projects.services')
+    .factory('ProjectsService', ProjectsService);
+
+  projectsService.$inject = ['$resource'];
+
+  function ProjectsService($resource) {
+    return $resource('api/projects/:projectId', {
+      projectId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+  }
+}());
