3461358729fd2499e8d6eb632479647cbe0d621a
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-06-21 15:57:18 +0100

    Update task model.

---
+++ modules/tasks/server/models/tasks.server.model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Task Schema
+ */
+var TaskSchema = new Schema({
+  name: {
+    type: String
+  },
+  owner: {
+    type: [{
+      type: Schema.Types.ObjectId
+    }]
+  },
+  author: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'active', 'complete']
+  }
+  /* Content */
+  desc: {
+    type: String,
+    default: ''
+  },
+  /* Timestamps */
+  updated: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('Task', TaskSchema);
