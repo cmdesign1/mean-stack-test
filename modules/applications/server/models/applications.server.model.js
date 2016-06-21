98a697559c3afa40fee1918717936401e4af8dca
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-06-21 15:34:19 +0100

    Update application model.

---
+++ modules/applications/server/models/applications.server.model.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Application Schema
+ */
+var ApplicationSchema = new Schema({
+  user: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  project: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Project'
+    }]
+  },
+  /* Content */
+  body: {
+    type: String,
+    default: ''
+  },
+  links: {
+    type: [{
+      type: String,
+      trim: true
+    }]
+  }
+  /* Timestamps */
+  updated: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  /* Open? */
+  open: {
+    type: Boolean
+  }
+});
+
+mongoose.model('Application', ApplicationSchema);
